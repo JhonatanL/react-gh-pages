24499925648fc0a190bf166b90d546de5a9d0533
JhonatanL <jhonatandef17@gmail.com>
2019-07-30 00:26:24 -0500

    Create a React app and publish it to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
+import Buscador from './components/Buscador';
+import Resultado from './components/Resultado';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    termino: '',
+    imagenes: [],
+    pagina: ''
+  }
+
+  scroll = () => {
+    const elemento = document.querySelector('.jumbotrom');
+    elemento.scrollIntoView('smooth','end');
+  }
+
+  paginaAnterior = () => {
+    // console.log('Anterior');
+    // Leer el state de la pagina actual
+    let pagina = this.state.pagina;
+
+    // Si la pagina es 1, ya no ir hacia atras
+    if(pagina === 1) return null;
+
+    //Restar uno a la pagina actual
+    pagina -= 1;
+
+    //agregar el cambio al state
+    this.setState({
+      pagina
+    }, () =>{
+      this.consultarApi();
+      this.scroll();
+    });
+
+    // console.log(pagina);
+  }
+  paginaSiguiente = () => {
+    // console.log('Siguiente...');
+    // Leer el state de la pagina actual
+    let pagina = this.state.pagina;
+
+    //sumar uno a la pagina actual
+    pagina += 1;
+    //agregar el cambio al state
+    this.setState({
+      pagina
+    }, () => {
+      this.consultarApi();
+      this.scroll();
+    });
+
+    // console.log(pagina);
+  }
+
+  consultarApi = () => {
+    const termino = this.state.termino;
+    const pagina = this.state.pagina;
+    const url = `https://pixabay.com/api/?key=13166503-f95feba3af70f876e20122795&q=${termino}&per_page=30&page=${pagina}`;
+    // console.log(url);
+    fetch(url)
+      .then(respuesta => respuesta.json())
+      .then(resultado => this.setState({imagenes: resultado.hits}))
+
+  }
+
+  datosBusqueda = (termino) => {
+    this.setState({
+      termino : termino,
+      pagina : 1
+    }, () => {
+      this.consultarApi();
+    })
+  }
+  render(){
+    return (
+      <div className="app container">
+        <div className="jumbotrom">
+          <p className="lead text-center">Buscador de Imagenes</p>
+          <Buscador 
+            datosBusqueda={this.datosBusqueda}
+          />
+        </div>
+        <div className="row justify-content-center">
+          <Resultado 
+            imagenes = {this.state.imagenes}
+            paginaAnterior = {this.paginaAnterior}
+            paginaSiguiente = {this.paginaSiguiente}
+          />
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/Buscador.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class Buscador extends Component {
+
+    BusquedaRef = React.createRef();
+    obtenerDatos = (e) => {
+        e.preventDefault();
+        //toma el valor del input
+        const termino = this.BusquedaRef.current.value;
+
+        // envia al componente principal
+        this.props.datosBusqueda(termino);
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.obtenerDatos}>
+                <div className="row">
+                    
+                    <div className="form-group col-md-8">
+                        <input ref={this.BusquedaRef} type="text" className="form-control form-control-lg" placeholder="Busca tu Imagen..." />
+                    </div>
+                    <div className="form-group col-md-4">
+                        <input type="submit" className="btn btn-lg btn-danger btn-block" value="Buscar"/>
+                    </div>
+                </div>
+            </form>
+        );
+    }
+}
+
+export default Buscador;
