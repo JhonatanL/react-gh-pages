637170366b613c4eb7e6e697a97e91bfb21f0768
JhonatanL <jhonatandef17@gmail.com>
2019-07-30 00:20:07 -0500

    Updates

---
+++ precache-manifest.36c10919c47b04876363e6026a284558.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00578d5fc0c41454fcf172c6104a10cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe35ba1d87e9f250f286",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "fe35ba1d87e9f250f286",
+    "url": "/static/js/main.5449d71c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
